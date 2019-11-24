4729f2054cc4a5a4b4651a0f33712cc9bac137df
marta1196 <34567942+marta1196@users.noreply.github.com>
2019-11-24 20:54:40 +0100

    métodos get y set.

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posición.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posición.java
@@ -4,4 +4,31 @@ public class Posición {
 
 	private int fila;
 	private char columna;
+
+	public int getFila() {
+		return fila;
+	}
+
+	private void setFila(int fila) {
+
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		} else {
+			this.fila = fila;
+		}
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		} else {
+			this.columna = columna;
+		}
+	}
+
 }
