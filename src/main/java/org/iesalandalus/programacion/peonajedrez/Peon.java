1ca38bb0145474a28c3bc66eb547c9c2d971227b
marta1196 <34567942+marta1196@users.noreply.github.com>
2019-11-26 12:32:42 +0100

    métodos get y set

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -1,11 +1,30 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 
+
 public class Peon {
 
 	private Color color;
 	private Posicion posicion;
 
+	public Color getColor() {
+		return color;
+	}
 
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		} else {
+			this.color = color;
+		}
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
 
 }
