54479809f7de2be61ab02294229f4a24c505fe05
marta1196 <34567942+marta1196@users.noreply.github.com>
2019-11-26 17:04:07 +0100

    Creación de la clase main con su atributo de  la calse peon y método mostrarpeon

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -1,12 +1,19 @@
 package org.iesalandalus.programacion.peonajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
-	
+
+	private static Peon peon;
+
 	public static void main(String[] args) {
-		System.out.println("Programa para aprender a colocar y mover un peón en el tablero de ajedrez");
-		System.out.println("-------------------------------------------------------------------------");
-		
-		System.out.println("Hasta luego Lucas!!!!");
+
 	}
-	
+
+	private static void mostrarPeon() {
+		System.out.println(peon.toString());
+	}
+
 }
