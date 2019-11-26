ccd7cd4b101dd5be37e5ef68de9c91aed26261ff
marta1196 <34567942+marta1196@users.noreply.github.com>
2019-11-26 12:44:56 +0100

    Métodos equals y hashCode

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -138,4 +138,32 @@ public class Peon {
 	}
 
 
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((posicion == null) ? 0 : posicion.hashCode());
+		return result;
+	}
+
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Peon other = (Peon) obj;
+		if (color != other.color)
+			return false;
+		if (posicion == null) {
+			if (other.posicion != null)
+				return false;
+		} else if (!posicion.equals(other.posicion))
+			return false;
+		return true;
+	}
+
+
 }
